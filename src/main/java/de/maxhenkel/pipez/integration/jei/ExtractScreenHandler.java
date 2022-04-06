11b918614dfdb41239255d8bc8a2b8dcee3f66ad
Max Henkel <max@maxhenkel.de>
2022-04-06 10:51:05 +0200

    Fix dependencies

--- src/main/java/de/maxhenkel/pipez/integration/jei/ExtractScreenHandler.java
+++ src/main/java/de/maxhenkel/pipez/integration/jei/ExtractScreenHandler.java
@@ -12,7 +12,7 @@ public class ExtractScreenHandler implements IGuiContainerHandler<ExtractScreen>
     @Override
     public List<Rect2i> getGuiExtraAreas(ExtractScreen screen) {
         if (screen.hasTabs()) {
-            return Collections.singletonList(new Rect2i(screen.getTabsX() - 10, screen.getTabsY(), screen.getTabsWidth(), screen.getTabsHeight() + 10));
+            return Collections.singletonList(new Rect2i(Math.max(screen.getTabsX() - 10, 0), screen.getTabsY(), screen.getTabsWidth(), screen.getTabsHeight() + 10));
         }
         return Collections.emptyList();
     }
