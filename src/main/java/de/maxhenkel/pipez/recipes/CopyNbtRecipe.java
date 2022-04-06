bd9708dfce758b0cc6b1cc3662a0dbb5b5e205ae
Max Henkel <max@maxhenkel.de>
2022-04-06 10:52:06 +0200

    Fix copy upgrade recipes Closes #54

--- src/main/java/de/maxhenkel/pipez/recipes/CopyNbtRecipe.java
+++ src/main/java/de/maxhenkel/pipez/recipes/CopyNbtRecipe.java
@@ -1,9 +1,12 @@
 package de.maxhenkel.pipez.recipes;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import de.maxhenkel.corelib.helpers.Pair;
 import de.maxhenkel.pipez.Main;
 import net.minecraft.core.NonNullList;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.Tag;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.inventory.CraftingContainer;
@@ -14,16 +17,21 @@ import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.registries.ForgeRegistryEntry;
 
+import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CopyNbtRecipe extends CustomRecipe {
 
-    private Ingredient ingredient;
+    private final Ingredient sourceIngredient;
+    private final Ingredient targetIngredient;
+    private final List<String> tags;
 
-    public CopyNbtRecipe(ResourceLocation recipeID, Ingredient ingredient) {
+    public CopyNbtRecipe(ResourceLocation recipeID, Ingredient sourceIngredient, Ingredient targetIngredient, List<String> tags) {
         super(recipeID);
-        this.ingredient = ingredient;
+        this.sourceIngredient = sourceIngredient;
+        this.targetIngredient = targetIngredient;
+        this.tags = tags;
     }
 
     public Pair<ItemStack, List<ItemStack>> getResult(CraftingContainer inv) {
@@ -34,15 +42,27 @@ public class CopyNbtRecipe extends CustomRecipe {
             ItemStack stack = inv.getItem(i);
             if (stack.isEmpty()) {
                 continue;
-            } else if (ingredient.test(stack)) {
-                if (source == null && stack.hasTag()) {
-                    source = stack;
-                } else {
-                    toCopy.add(stack);
-                }
-            } else {
+            }
+            boolean matchesSource = sourceIngredient.test(stack);
+            boolean matchesTarget = targetIngredient.test(stack);
+
+            if (!matchesSource && !matchesTarget) {
                 return new Pair<>(null, new ArrayList<>());
             }
+
+            if (matchesSource) {
+                if (source == null && stack.hasTag()) {
+                    source = stack;
+                    continue;
+                }
+            }
+            if (matchesTarget) {
+                if (!stack.hasTag()) {
+                    toCopy.add(stack);
+                    continue;
+                }
+            }
+            return new Pair<>(null, new ArrayList<>());
         }
         return new Pair<>(source, toCopy);
     }
@@ -51,16 +71,11 @@ public class CopyNbtRecipe extends CustomRecipe {
     public boolean matches(CraftingContainer inv, Level worldIn) {
         Pair<ItemStack, List<ItemStack>> result = getResult(inv);
 
-        if (result.getKey() == null) {
+        if (result.getKey() == null || result.getValue().isEmpty()) {
             return false;
         }
 
-        if (result.getValue().stream().allMatch(stack -> stack.getItem().equals(result.getKey().getItem()))) {
-            return result.getKey() != null && result.getValue().size() > 0;
-        } else if (result.getValue().size() == 1) {
-            return true;
-        }
-        return false;
+        return result.getValue().size() == 1 || result.getValue().stream().allMatch(stack -> stack.getItem().equals(result.getKey().getItem()));
     }
 
     @Override
@@ -77,11 +92,48 @@ public class CopyNbtRecipe extends CustomRecipe {
         } else if (result.getValue().size() == 1) {
             ItemStack stack = result.getValue().get(0).copy();
             stack.setCount(1);
-            stack.setTag(result.getKey().getTag().copy());
+            CompoundTag tag = result.getKey().getTag();
+            CompoundTag copy = new CompoundTag();
+            for (String s : tags) {
+                Tag element = getPath(s, tag);
+                if (element != null) {
+                    setPath(s, copy, element);
+                }
+            }
+            stack.setTag(copy);
             return stack;
         }
 
         return ItemStack.EMPTY;
+    }
+
+    @Nullable
+    private Tag getPath(String path, CompoundTag tag) {
+        String[] p = path.split("\\.");
+        CompoundTag c = tag;
+        for (int i = 0; i < p.length - 1; i++) {
+            if (c.contains(p[i], Tag.TAG_COMPOUND)) {
+                c = c.getCompound(p[i]);
+            } else {
+                return null;
+            }
+        }
+        return c.get(p[p.length - 1]);
+    }
+
+    private void setPath(String path, CompoundTag tag, Tag element) {
+        String[] p = path.split("\\.");
+        CompoundTag c = tag;
+        for (int i = 0; i < p.length - 1; i++) {
+            if (c.contains(p[i], Tag.TAG_COMPOUND)) {
+                c = c.getCompound(p[i]);
+            } else {
+                CompoundTag newTag = new CompoundTag();
+                c.put(p[i], newTag);
+                c = newTag;
+            }
+        }
+        c.put(p[p.length - 1], element);
     }
 
     @Override
@@ -129,17 +181,34 @@ public class CopyNbtRecipe extends CustomRecipe {
 
         @Override
         public CopyNbtRecipe fromJson(ResourceLocation recipeId, JsonObject json) {
-            return new CopyNbtRecipe(recipeId, Ingredient.fromJson(json.get("item")));
+            List<String> tags = new ArrayList<>();
+            if (json.has("tags")) {
+                JsonArray tagArray = json.get("tags").getAsJsonArray();
+                for (int i = 0; i < tagArray.size(); i++) {
+                    tags.add(tagArray.get(i).getAsString());
+                }
+            }
+            return new CopyNbtRecipe(recipeId, Ingredient.fromJson(json.get("source")), Ingredient.fromJson(json.get("target")), tags);
         }
 
         @Override
         public CopyNbtRecipe fromNetwork(ResourceLocation recipeId, FriendlyByteBuf buffer) {
-            return new CopyNbtRecipe(recipeId, Ingredient.fromNetwork(buffer));
+            ArrayList<String> tags = new ArrayList<>();
+            int tagSize = buffer.readVarInt();
+            for (int i = 0; i < tagSize; i++) {
+                tags.add(buffer.readUtf());
+            }
+            return new CopyNbtRecipe(recipeId, Ingredient.fromNetwork(buffer), Ingredient.fromNetwork(buffer), tags);
         }
 
         @Override
         public void toNetwork(FriendlyByteBuf buffer, CopyNbtRecipe recipe) {
-            recipe.ingredient.toNetwork(buffer);
+            buffer.writeVarInt(recipe.tags.size());
+            for (String s : recipe.tags) {
+                buffer.writeUtf(s);
+            }
+            recipe.sourceIngredient.toNetwork(buffer);
+            recipe.targetIngredient.toNetwork(buffer);
         }
     }
 }
